b6a6275ba27f4d667d1ac8ed93153010ffcd20ac
Abdul-samad Ajibodu <ajibodua@sheridancollege.ca>
2025-01-24 14:22:08 -0500

    added a space before the exiting program

--- Program.cs
+++ Program.cs
@@ -5,7 +5,9 @@
         Console.WriteLine("Hello, C# World!");
         Console.WriteLine("Press any key to exit...");
         Console.ReadKey();
+        Console.WriteLine(" ");
         Console.WriteLine("exiting program...");
+        
     }
 }
 
